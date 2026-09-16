2dac83a018368715487e270a424f0373fda17d49
Enad-Alharbi <enad_alkhtabi@hotmail.com>
2026-09-16 11:51:25 +0300

    feat: setup Angular project structure and routing

--- Frontend/TaskatiApp.Web/src/app/app.routes.ts
+++ Frontend/TaskatiApp.Web/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: 'tasks',
+		loadChildren: () =>
+			import('./features/tasks/tasks.routes').then((module) => module.TASKS_ROUTES),
+	},
+	{
+		path: 'categories',
+		loadChildren: () =>
+			import('./features/categories/categories.routes').then(
+				(module) => module.CATEGORIES_ROUTES,
+			),
+	},
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'tasks',
+	},
+	{
+		path: '**',
+		redirectTo: 'tasks',
+	},
+];
---
+++ Frontend/TaskatiApp.Web/src/app/features/tasks/index.ts
@@ -0,0 +1,1 @@
+export { TASKS_ROUTES } from './tasks.routes';
